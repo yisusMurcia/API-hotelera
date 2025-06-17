2362be2829d29b89bea8788e3afe0733e16da6ad
Jesus Antonio Murcia <yisusmurci@gmail.com>
2025-06-16 19:46:19 -0500

    Project created

---
+++ src/main/java/ing/yisus/apihotelera/ApiHoteleraApplication.java
@@ -0,0 +1,13 @@
+package ing.yisus.apihotelera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHoteleraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiHoteleraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ing/yisus/apihotelera/ApiHoteleraApplicationTests.java
@@ -0,0 +1,13 @@
+package ing.yisus.apihotelera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHoteleraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
